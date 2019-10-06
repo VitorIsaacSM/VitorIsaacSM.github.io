19a17dcb59e0f65815aecd7bdb3d3353b71d189b
vitor isaac sitton maia <vitor2001bidi@gmail.com>
2019-10-06 01:22:22 -0300

    cria aba Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'sobre', component: HomeComponent},
+  {path: 'projetos', component: HomeComponent},
+  {path: 'contato', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
